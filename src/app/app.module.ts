f8040998a41d985eaffd4b52ecb44ad91b0fe6f9
Romain Giroux <romain.giroux@outlook.fr>
2022-10-12 20:41:13 +0200

    Ajouter les premières features Formation, Expérience

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormationsComponent } from './formations/formations.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormationsComponent,
+    ExperienceComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
